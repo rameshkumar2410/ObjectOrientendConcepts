fc57d689efeda27f16e6c53d9eb31baca955dc87
Ramesh <Ramesh@Ramesh-PC>
2019-10-26 19:55:35 +0800

    Modified

--- src/com/org/oopconcept/countinganimal/CountingAnimal.java
+++ src/com/org/oopconcept/countinganimal/CountingAnimal.java
@@ -6,7 +6,7 @@ public class CountingAnimal extends Animal {
 	private int walk = 0;
 	private static int sing = 0;
 	private int swim = 0;
-	Animal animal=new Animal();
+	Animal animal = new Animal();
 
 	public void getCountofAnimalActivities() {
 
@@ -17,18 +17,17 @@ public class CountingAnimal extends Animal {
 
 		if (getBird().sing().equals("I am singing") || getDuck().getSound().equals("I am singing")
 				|| getChicken().getSound().equals("I am singing") || getRooster().getSound().equals("I am singing")
-				|| getFish().sing().equals("I am singing")
-				|| getShark().sing().equals("I am singing") || getClown().sing().equals("I am singing")
-				|| getDol().sing().equals("I am singing") || getDog().parrotSays().equals("I am singing")
-				|| getButt().sound().equals("I am singing")) {
+				|| getFish().sing().equals("I am singing") || getShark().sing().equals("I am singing")
+				|| getClown().sing().equals("I am singing") || getDol().sing().equals("I am singing")
+				|| getDog().parrotSays().equals("I am singing") || getButt().sound().equals("I am singing")) {
 			sing++;
 		}
 
 	}
 
 	public static void main(String arg[]) {
-		
-		CountingAnimal animal=new CountingAnimal();
+
+		CountingAnimal animal = new CountingAnimal();
 		animal.getCountofAnimalActivities();
 		System.out.println(fly);
 		System.out.println(sing);
